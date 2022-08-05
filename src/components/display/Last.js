58cb2d1a9bceed36c7bc1427b20a1e9c1d6d6298
lUCI <lusindisontanjana@gmail.com>
2022-08-05 07:53:58 +0200

    Fix design

--- src/components/display/Last.js
+++ src/components/display/Last.js
@@ -1,18 +1,18 @@
-import React, { PureComponent } from "react";
-import Button from "../Button";
-import "./Last.css";
+import React, { PureComponent } from 'react';
+import Button from '../Button';
+import './Last.css';
 
-const list = ["÷", "X", "-", "+", "="];
+const list = ['÷', 'X', '-', '+', '='];
 class Last extends PureComponent {
-	render() {
-		return (
-			<div className="last">
-				{list.map((el) => (
-					<Button value={el} key={el} />
-				))}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="last">
+        {list.map((el) => (
+          <Button value={el} key={el} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Last;
