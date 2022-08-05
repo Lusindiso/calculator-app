17a9286332f05e852cfd47562bc0aba9babb1002
lUCI <lusindisontanjana@gmail.com>
2022-08-05 10:14:32 +0200

    fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,39 @@
-import React, { PureComponent } from 'react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
 import Buttons from './display/Buttons';
 import './Calculator.css';
 import Last from './display/Last';
 import Screen from './Screen';
+import calculate from '../logic/calculate';
 
-class Calculator extends PureComponent {
-  render() {
-    return (
-      <div className="calculator">
-        <Screen />
-        <div className="bottom">
-          <Last />
-          <Buttons />
-        </div>
-      </div>
-    );
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: 0,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+	handleClick = (btnName) => {
+	  this.setState((prevState) => calculate(prevState, btnName));
+	};
+
+	render() {
+	  return (
+  <div className="calculator">
+    <Screen value={this.state} />
+    <div className="bottom">
+      <Last handleClick={this.handleClick} />
+      <Buttons handleClick={this.handleClick} />
+    </div>
+  </div>
+	  );
+	}
 }
 
 export default Calculator;
